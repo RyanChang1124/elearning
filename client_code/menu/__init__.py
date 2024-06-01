6473409b50349a502285ed6e73e79fd9c89fcee0
ryanchangkhaiyang@gmail.com <ryanchangkhaiyang@gmail.com>
2024-06-01 19:22:29 +0000

    Created custom package dependencies, menu, menu.lecpage, menu.stupage and 3 more; Edited settings, ServerModule1 and theme/parameters.yaml; Deleted login.Form1 and login

---
+++ client_code/menu/__init__.py
@@ -0,0 +1,29 @@
+from ._anvil_designer import menuTemplate
+from anvil import *
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.users
+import anvil.server
+
+def loginadd():
+  anvil.server.call('metricdateincrement')
+class menu(menuTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    anvil.users.login_with_form()
+    if anvil.users.get_user()['setup_complete'] is None:
+      open_form('newuser')
+    elif anvil.users.get_user()['setup_complete'] is True:
+      if anvil.users.get_user()['role'] == 'Student':
+        open_form('menu.stupage')
+      elif anvil.users.get_user()['role'] == 'Lecturer':
+        open_form('menu.lecpage')
--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -1,5 +1,11 @@
+import anvil.google.auth, anvil.google.drive, anvil.google.mail
+from anvil.google.drive import app_files
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.users
 import anvil.server
-
+import pandas as pd
 # This is a server module. It runs on the Anvil server,
 # rather than in the user's browser.
 #
@@ -12,3 +18,21 @@ import anvil.server
 #   print("Hello, " + name + "!")
 #   return 42
 #
+@anvil.server.callable
+def metricdateincrement():
+  dates = [r['date'] for r in app_tables.metrics.search()]
+  today = pd.to_datetime('today').normalize
+  if today in dates:
+    metric = app_tables.metrics.get(date=today)
+    metric['NumberOfLogins'] += 1
+  else:
+    app_tables.metrics.add_row(
+    date=today,
+    logins= 1)
+
+@anvil.server.callable
+def checkvalueusers(value):
+  return [r[value] for r in app_tables.users.search()]
+@anvil.server.callable
+def newuservalue(column,value):
+  anvil.users.get_user()[column] = value
