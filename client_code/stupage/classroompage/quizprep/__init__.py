f48902e3ccc54cbe7d960720f1ff27adaf759678
ryanchangkhaiyang@gmail.com <ryanchangkhaiyang@gmail.com>
2024-06-03 10:30:21 +0000

    Edited stupage.classroompage.quizprep and stupage.classlist

--- client_code/stupage/classroompage/quizprep/__init__.py
+++ client_code/stupage/classroompage/quizprep/__init__.py
@@ -7,11 +7,20 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-
+quizcodenum = None
+user = None
 
 class quizprep(quizprepTemplate):
-  def __init__(self, **properties):
+  def __init__(self,quizcode,username, **properties):
+    global quizcodenum 
+    global user
+    user = username
+    quizcodenum = quizcode
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
+    
     # Any code you write here will run before the form opens.
+
+  def outlined_button_2_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    anvil.open_form('stupage.classroompage.QuizForm',quizcodenum,user)
