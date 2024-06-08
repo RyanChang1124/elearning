4a743f3dcd9d30df6c77e6ffa0e5110b66aec585
ryanchangkhaiyang@gmail.com <ryanchangkhaiyang@gmail.com>
2024-06-08 07:11:17 +0000

    Created lecpage.lecclasspage.quizedit.ItemTemplate6; Edited lecpage.lecclasspage.quizedit

--- client_code/lecpage/lecclasspage/quizedit/ItemTemplate6/__init__.py
+++ client_code/lecpage/lecclasspage/quizedit/ItemTemplate6/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import quizeditTemplate
+from ._anvil_designer import ItemTemplate6Template
 from anvil import *
 import anvil.server
 import anvil.google.auth, anvil.google.drive
@@ -9,9 +9,9 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class quizedit(quizeditTemplate):
+class ItemTemplate6(ItemTemplate6Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.drop_down_1.items = ['A', 'B', 'C', 'D']
+
     # Any code you write here will run before the form opens.
