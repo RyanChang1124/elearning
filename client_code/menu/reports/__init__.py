dfedc0bca1bed762e64e60a1a09ae09dfd3b70b8
ryanchangkhaiyang@gmail.com <ryanchangkhaiyang@gmail.com>
2024-06-09 14:17:33 +0000

    Edited menu.reports and menu.reports.ItemTemplate6

--- client_code/menu/reports/__init__.py
+++ client_code/menu/reports/__init__.py
@@ -12,6 +12,10 @@ from anvil.tables import app_tables
 class reports(reportsTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
+    reports = app_tables.quizzes.search(reports=anvil.tables.number_greater_than(0))
+    
+    # Set the items of the RepeatingPanel
+    self.repeating_panel_1.items = reports
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
