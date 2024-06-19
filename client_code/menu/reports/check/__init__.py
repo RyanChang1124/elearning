d50936d506f7677ffa3706b5d50670b8b28b3b84
ryanchangkhaiyang@gmail.com <ryanchangkhaiyang@gmail.com>
2024-06-19 13:32:12 +0000

    Created menu.reports.check and menu.reports.check.ItemTemplate13; Edited menu.reports.ItemTemplate6

---
+++ client_code/menu/reports/check/__init__.py
@@ -0,0 +1,40 @@
+from ._anvil_designer import checkTemplate
+from anvil import *
+import anvil.server
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+
+class check(checkTemplate):
+  def __init__(self,quizid, **properties):
+    self.quizcode = quizid
+    self.label_1.text = app_tables.quizzes.get(quizcode=quizid)['quizname']
+    # Set Form properties and Data Bindings.
+    self.repeating_panel_1.items = app_tables.quizcontent.get(quizcode=quizid)
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+
+  def outlined_button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    target = app_tables.quizcontent.search(quizcode=self.quizcode)
+    target.delete()
+    target = app_tables.quizzes.search(quizcode=self.quizcode)
+    target.delete()
+    target = app_tables.quizresult.search(quizcode=self.quizcode)
+    target.delete()
+    open_form('menu.reports')
+
+  def outlined_button_2_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    target = app_tables.quizzes.get(quizcode=self.quizcode)
+    target.update(reports=0)
+    open_form('menu.reports')
+
+  def button_2_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('menu.reports')
